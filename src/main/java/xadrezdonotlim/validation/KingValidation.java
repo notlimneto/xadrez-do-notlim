d2d7f7e3b07220224a731ce090418a7131d21e70
notlimneto <milton.carvalhaes40@gmail.com>
2024-11-29 01:54:20 -0300

    feat(King): Chegou o 01 (com ele muitos problemas para implementar mate)

---
+++ src/main/java/xadrezdonotlim/validation/KingValidation.java
@@ -0,0 +1,30 @@
+package xadrezdonotlim.validation;
+
+import xadrezdonotlim.domain.Board;
+import xadrezdonotlim.enumeration.PositionIdentifiersEnum;
+
+public class KingValidation {
+    public static boolean moveValidation(Board board, String currentPosition, String nextPosition, char color) {
+        var positionMap = board.getBoard();
+        String columns = PositionIdentifiersEnum.COLUMNS.getValues();
+
+        String currentColumn = currentPosition.substring(0, 1);
+        Integer currentRow = Integer.valueOf(currentPosition.substring(1));
+
+        String nextColumn = nextPosition.substring(0, 1);
+        Integer nextRow = Integer.valueOf(nextPosition.substring(1));
+
+        Integer indexOfCurrentColumn = columns.indexOf(currentColumn);
+        Integer indexOfNextColumn = columns.indexOf(nextColumn);
+
+        boolean movementCondition = Math.abs(nextRow - currentRow) == 1 || Math.abs(indexOfNextColumn - indexOfCurrentColumn) == 1;
+
+        if (!movementCondition) return false;
+
+        boolean hasSameColorPieceOnNextSquare = positionMap.get(nextPosition) != null && positionMap.get(nextPosition).getColor() == color;
+
+        if (hasSameColorPieceOnNextSquare) return false;
+
+        return true;
+    }
+}
